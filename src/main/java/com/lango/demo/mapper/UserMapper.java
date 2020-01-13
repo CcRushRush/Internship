8a9f7ab2effa78c346993e36de9dc59640dc2d42
cc-djy <836633504@qq.com>
2020-01-13 20:22:34 +0800

    数据库连接

--- src/main/java/com/lango/demo/mapper/UserMapper.java
+++ src/main/java/com/lango/demo/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.lango.demo.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lango.demo.pojo.User;
@@ -7,5 +9,5 @@ import com.lango.demo.pojo.User;
 @Repository
 public interface UserMapper {
 
-	public User login();
+	public List<User> selectUsers();
 }
